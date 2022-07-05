1ca1a3b3dc789a5bb325beed87490b6df5a44e25
David Liman <limanoit@gmail.com>
2022-07-05 11:01:18 -0700

    add data models

--- src/k1/core.clj
+++ src/k1/core.clj
@@ -18,13 +18,58 @@
 
 (xt/entity (xt/db node) :manifest)
 
-;; https://kennytilton.github.io/whoishiring/
-;; filters: remote, onsite (location), interns (job type), visa (immigration)
-;; filters: starred, noted, applied, excluded, unreviewed (personal application)
-;; sort: creation, stars, company
-;; search: title only, full listing
-;; role: full-stack | backend |
-;; edition: june 2022
-;; headquarter, job position, remote
+;; site/id
+;; site/name
+;; site/domain
+;; site/navigation
+;; site/classless-css
+;; site/classless-theme
+;; site/network-enabled
+;; site/date-formatter-format
+;; site/footer
 ;;
-;; launch yc
+;; site/get-site-id [access-token]
+;; auth/get-user-info [access-token]
+;; auth/login [email] generate-session, ses-auth-login, aws-user-pool/validates, possibly callback, write cookie/access-token
+;; auth/logout
+;;
+;; UserInfo writes post associated to site
+;; post/owner-id (user-info/id)
+;; post/site-id
+;; post/id
+;; post/title
+;; post/content
+;; post/created-at
+;; post/updated-at
+;;
+;; site-homepage
+;; subdomain (site/domain) -> (site/id, post/site-id) -> (site/*, post/* created-at desc)
+;;
+;; post-detail
+;; post/slug -> post/*
+;;
+;; admin
+;;   admin-homepage
+;;     manage
+;;       pages
+;;       navigation
+;;       posts
+;;
+;;     site
+;;       design (classless example, link to site with examples markdown)
+;;       domain
+;;       network
+;;       delete this space
+;;
+;; You are logged in. This means you will be able to see draft blog posts.
+;; If you want to see the site as it appears to everyone else, you may logout.
+;;
+;; public
+;; landing page
+;;    features
+;;      minimalistic design, no tracking
+;;      rss
+;;      free custom domain (learn more)
+
+(defn main []
+  )
